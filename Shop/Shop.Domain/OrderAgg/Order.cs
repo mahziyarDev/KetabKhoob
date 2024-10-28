d6251cb4aad7654326753a48e3bdd2cf4dcb5ecf
mahziyar dehghan <mahziyar138006@gmail.com>
2024-10-29 00:44:34 +0330

    add : methods in Order

--- Shop/Shop.Domain/OrderAgg/Order.cs
+++ Shop/Shop.Domain/OrderAgg/Order.cs
@@ -11,10 +11,32 @@ namespace Shop.Domain.OrderAgg
         public OrderStatus OrderStatus { get; private set; }
         public OrderDiscount? Discount { get; private set; }
         public List<OrderItem> Items { get; private set; }
+        public int TotalPrice => Items.Sum(x => x.TotalPrice);
+        public int ItemCount => Items.Count();
+
 
     }
-    public class OrderItem : BaseEntity 
+    public class OrderItem : BaseEntity
     {
+        public OrderItem(long inventoryId, int count, int price)
+        {
+            InventoryId = inventoryId;
+            Count = count;
+            Price = price;
+        }
+
         public long OrderId { get; internal set; }
+        public long InventoryId { get; private set; }
+        public int Count { get; private set; }
+        public int Price { get; private set; }
+        public int TotalPrice => TotalPrice * Price;
+        
+        public void ChangeCount(int newCount)
+        {
+            if (newCount < 1)
+                return;
+            Count = newCount;
+        }
+
     }
 }
